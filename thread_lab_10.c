fe627802efb0374dac8704a4a5288af9877994ea
Iqbal <101163068+IqbalMahbub@users.noreply.github.com>
2025-07-12 10:41:50 +0600

    Add files via upload

--- thread_lab_10.c
+++ thread_lab_10.c
@@ -5,16 +5,19 @@
 void *thread_function(void *arg){
 
 	printf("Inside Thread\n");
-	
+	for(int i=0;i<5;i++){
+		printf("%d\n",i);
+		sleep(1);
+	}	
 }
 
 int main(){
 
 	pthread_t a_thread;
-	pthread_creat(&a_thread,NULL,thread_function,NULL);
+	pthread_create(&a_thread,NULL,thread_function,NULL);
 	pthread_join(a_thread,NULL);
 	printf("Inside main program\n");
-	for(j=20;j<25;j++){
+	for(int j=20;j<25;j++){
 		printf("%d\n\n",j);
 		sleep(1);
 
